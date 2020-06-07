9766dc1a24bb539b28c287dde88d77a11b20ea20
WalkerHC <walker.hc30@gmail.com>
2020-06-07 17:01:23 +1000

    add check file

--- products.py
+++ products.py
@@ -1,12 +1,15 @@
-#讀取檔案
-products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-    for line in f:
-        if '商品,價格' in line:
-        	continue # 繼續 (跳到下一回)
-        name, price = line.strip().split(',')
-        products.append([name, price])
+import os #載入os
 
+products = []
+# 讀取檔案
+if os.path.isfile('products.csv'): # 檢查檔案在不在
+    print('找到檔案了!')
+    with open('products.csv', 'r', encoding='utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue # 繼續 (跳到下一回)
+            name, price = line.strip().split(',')
+            products.append([name, price])
 print(products)
 
 # 讓使用者輸入
@@ -16,7 +19,7 @@ while True:
         break
     price = input('請輸入價格: ')
     price = int(price)
-    products.append([name, price])
+    products.append([name, price]) # name & price 做成小清單放到products的大清單
 print(products)
 
 # 印出所有購買紀錄
